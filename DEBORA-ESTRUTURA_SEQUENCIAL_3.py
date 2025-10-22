9d253faafdace592ce88255fd6020c5b7ab28735
deborazs <debora_bs_santos@estudante.sesisenai.org.br>
2025-10-21 21:56:42 -0300

    DB

---
+++ DEBORA-ESTRUTURA_SEQUENCIAL_3.py
@@ -0,0 +1,6 @@
+#Faça um programa que peça dois números e imprima a soma:
+
+numero1=float(input("Digite o numero 1: "))
+numero2=float(input("Digite o número 2: "))
+soma=(numero1+numero2)
+print("Soma é:",soma)

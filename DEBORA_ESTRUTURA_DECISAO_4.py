b12155793f0d5bf83e492bd1524d12227a72fc71
deborazs <debora_bs_santos@estudante.sesisenai.org.br>
2025-10-27 19:45:08 -0300

    DB

---
+++ DEBORA_ESTRUTURA_DECISAO_4.py
@@ -0,0 +1,10 @@
+#Faça um programa que verifique se uma letra digitada é vogal ou consoante.
+#desenvolvido por : Debora
+
+letra=input("Escreva uma letra:")
+#if:se 
+if letra and "aeiou":
+    print("Essa letra é uma vogal.")
+#senao
+else:
+    print("Essa letra é uma consoante.")

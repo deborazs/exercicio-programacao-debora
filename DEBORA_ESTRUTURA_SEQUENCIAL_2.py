2d33153ae2e8fa193d2ab46b861806dd60140b35
deborazs <debora_bs_santos@estudante.sesisenai.org.br>
2025-10-28 20:52:23 -0300

    DB

--- DEBORA_ESTRUTURA_SEQUENCIAL_2.py
+++ DEBORA_ESTRUTURA_SEQUENCIAL_2.py
@@ -1,5 +1,5 @@
 #Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
 #Desenvolvido por: Debora 
 
-número = input("Digite um número: ")
-print("O número informado foi",número)
+numero = input("Digite um número: ")
+print("O número informado foi",numero)
